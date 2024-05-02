e20e492e0efe648a2a074d60dfac2fdea9058a03
Kris Nuttycombe <kris@nutty.land>
2024-05-02 11:38:00 -0600

    WIP

--- zcash_client_sqlite/src/testing/pool.rs
+++ zcash_client_sqlite/src/testing/pool.rs
@@ -328,6 +328,188 @@ pub(crate) fn send_single_step_proposed_transfer<T: ShieldedPoolTester>() {
     );
 }
 
+pub(crate) fn send_multi_output_proposed_transfer<T: ShieldedPoolTester>() {
+    let mut st = TestBuilder::new()
+        .with_block_cache()
+        .with_account_from_sapling_activation(BlockHash([0; 32]))
+        .build();
+
+    let account = st.test_account().cloned().unwrap();
+    let dfvk = T::test_account_fvk(&st);
+
+    // Add funds to the wallet in a single note
+    let value = NonNegativeAmount::const_from_u64(60000);
+    let (h, _, _) = st.generate_next_block(&dfvk, AddressType::DefaultExternal, value);
+    st.scan_cached_blocks(h, 1);
+
+    // Spendable balance matches total balance
+    assert_eq!(st.get_total_balance(account.account_id()), value);
+    assert_eq!(st.get_spendable_balance(account.account_id(), 1), value);
+
+    assert_eq!(
+        block_max_scanned(&st.wallet().conn, &st.wallet().params)
+            .unwrap()
+            .unwrap()
+            .block_height(),
+        h
+    );
+
+    let to_extsk = T::sk(&[0xf5; 32]);
+    let to: Address = T::sk_default_address(&to_extsk);
+    let request = zip321::TransactionRequest::new(vec![Payment::without_memo(
+        to.to_zcash_address(&st.network()),
+        NonNegativeAmount::const_from_u64(10000),
+    )])
+    .unwrap();
+
+    // TODO: This test was originally written to use the pre-zip-313 fee rule
+    // and has not yet been updated.
+    #[allow(deprecated)]
+    let fee_rule = StandardFeeRule::PreZip313;
+
+    let change_memo = "Test change memo".parse::<Memo>().unwrap();
+    let change_strategy = standard::SingleOutputChangeStrategy::new(
+        fee_rule,
+        Some(change_memo.clone().into()),
+        T::SHIELDED_PROTOCOL,
+    );
+    let input_selector = &GreedyInputSelector::new(change_strategy, DustOutputPolicy::default());
+
+    let proposal = st
+        .propose_transfer(
+            account.account_id(),
+            input_selector,
+            request,
+            NonZeroU32::new(1).unwrap(),
+        )
+        .unwrap();
+
+    let create_proposed_result = st.create_proposed_transactions::<Infallible, _>(
+        account.usk(),
+        OvkPolicy::Sender,
+        &proposal,
+    );
+    assert_matches!(&create_proposed_result, Ok(txids) if txids.len() == 1);
+
+    let sent_tx_id = create_proposed_result.unwrap()[0];
+
+    // Verify that the sent transaction was stored and that we can decrypt the memos
+    let tx = st
+        .wallet()
+        .get_transaction(sent_tx_id)
+        .unwrap()
+        .expect("Created transaction was stored.");
+    let ufvks = [(
+        account.account_id(),
+        account.usk().to_unified_full_viewing_key(),
+    )]
+    .into_iter()
+    .collect();
+    let d_tx = decrypt_transaction(&st.network(), h + 1, &tx, &ufvks);
+    assert_eq!(T::decrypted_pool_outputs_count(&d_tx), 2);
+
+    let mut found_tx_change_memo = false;
+    let mut found_tx_empty_memo = false;
+    T::with_decrypted_pool_memos(&d_tx, |memo| {
+        if Memo::try_from(memo).unwrap() == change_memo {
+            found_tx_change_memo = true
+        }
+        if Memo::try_from(memo).unwrap() == Memo::Empty {
+            found_tx_empty_memo = true
+        }
+    });
+    assert!(found_tx_change_memo);
+    assert!(found_tx_empty_memo);
+
+    // Verify that the stored sent notes match what we're expecting
+    let sent_note_ids = {
+        let mut stmt_sent_notes = st
+            .wallet()
+            .conn
+            .prepare(
+                "SELECT output_index
+                FROM sent_notes
+                JOIN transactions ON transactions.id_tx = sent_notes.tx
+                WHERE transactions.txid = ?",
+            )
+            .unwrap();
+
+        stmt_sent_notes
+            .query(rusqlite::params![sent_tx_id.as_ref()])
+            .unwrap()
+            .mapped(|row| Ok(NoteId::new(sent_tx_id, T::SHIELDED_PROTOCOL, row.get(0)?)))
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap()
+    };
+
+    assert_eq!(sent_note_ids.len(), 2);
+
+    // The sent memo should be the empty memo for the sent output, and the
+    // change output's memo should be as specified.
+    let mut found_sent_change_memo = false;
+    let mut found_sent_empty_memo = false;
+    for sent_note_id in sent_note_ids {
+        match st
+            .wallet()
+            .get_memo(sent_note_id)
+            .expect("Note id is valid")
+            .as_ref()
+        {
+            Some(m) if m == &change_memo => {
+                found_sent_change_memo = true;
+            }
+            Some(m) if m == &Memo::Empty => {
+                found_sent_empty_memo = true;
+            }
+            Some(other) => panic!("Unexpected memo value: {:?}", other),
+            None => panic!("Memo should not be stored as NULL"),
+        }
+    }
+    assert!(found_sent_change_memo);
+    assert!(found_sent_empty_memo);
+
+    // Check that querying for a nonexistent sent note returns None
+    assert_matches!(
+        st.wallet()
+            .get_memo(NoteId::new(sent_tx_id, T::SHIELDED_PROTOCOL, 12345)),
+        Ok(None)
+    );
+
+    let tx_history = st.get_tx_history().unwrap();
+    assert_eq!(tx_history.len(), 2);
+
+    assert_eq!(
+        tx_history[0].account_value_delta(),
+        ZatBalance::const_from_i64(60000)
+    );
+    let tx_0_out = st.get_tx_outputs(tx_history[0].txid()).unwrap();
+    assert_eq!(tx_0_out.len(), 1);
+    assert_eq!(tx_0_out[0].value(), Zatoshis::const_from_u64(60000));
+
+    assert_eq!(
+        tx_history[1].account_value_delta(),
+        ZatBalance::const_from_i64(-20000)
+    );
+    let tx_1_out = st.get_tx_outputs(tx_history[1].txid()).unwrap();
+    assert_eq!(tx_1_out.len(), 2);
+    assert_eq!(tx_1_out[0].value(), Zatoshis::const_from_u64(10000));
+    assert_matches!(tx_1_out[0].recipient(), Recipient::External(_, _));
+    assert_eq!(tx_1_out[1].value(), Zatoshis::const_from_u64(40000));
+    assert_matches!(
+        tx_1_out[1].recipient(),
+        Recipient::InternalAccount {
+            external_address: None,
+            ..
+        }
+    );
+
+    assert_matches!(
+        decrypt_and_store_transaction(&st.network(), st.wallet_mut(), &tx),
+        Ok(_)
+    );
+}
+
+
 #[cfg(feature = "transparent-inputs")]
 pub(crate) fn send_multi_step_proposed_transfer<T: ShieldedPoolTester>() {
     use std::{collections::HashSet, str::FromStr};
